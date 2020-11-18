37b9cc3a4685965fca505aa4b524d723fc8b197b
ivansheshin <ivansheshin@gmail.com>
2020-11-18 20:08:00 +0300

    Добавление ассоциативного массива с данными о товаре, его категории и цене

--- data.php
+++ data.php
@@ -10,3 +10,11 @@ $is_auth = (bool) rand(0, 1);
 
 $user_name = 'Константин';
 $user_avatar = 'img/user.jpg';
+$price_list = [
+    ['title' => '2014 Rossignol District Snowboard', 'category' => 'Доски и лыжи', 'price' => 10999],
+    ['title' => '2DC Ply Mens 2016/2017 Snowboard', 'category' => 'Доски и лыжи', 'price' => 159999],
+    ['title' => 'Крепления Union Contact Pro 2015 года размер L/XL', 'category' => 'Крепления', 'price' => 8000],
+    ['title' => 'Ботинки для сноуборда DC Mutiny Charocal', 'category' => 'Ботинки', 'price' => 10999],
+    ['title' => 'Куртка для сноуборда DC Mutiny Charocal', 'category' => 'Одежда', 'price' => 7500],
+    ['title' => 'Маска Oakley Canopy', 'category' => 'Разное', 'price' => 5400],
+];
